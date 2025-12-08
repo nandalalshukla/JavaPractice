a76bbe1bf34fd01539623c932ac8d8a70679abe3
Nandalal Shukla <shuklanandalal@gmail.com>
2025-12-08 22:54:49 +0530

    first commit

---
+++ main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        // This is a simple Java program that prints "Hello, World!" to the console.
+        System.out.println("Hello, World!");
+    }
+}
