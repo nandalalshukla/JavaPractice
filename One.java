0bf939ec865f511c264d09d4f0e25c4498917850
Nandalal Shukla <shuklanandalal@gmail.com>
2025-12-09 00:57:34 +0530

    did one java program target-100 question in 24hrs

---
+++ One.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class One {
+    public static void main(String[] args) {
+        
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter the details");
+        System.out.println("Enter name");
+        String name = inp.nextLine();
+        System.out.println("Enter age");
+        int age = inp.nextInt();
+        System.out.println("Enter cgpa");
+        double cgpa = inp.nextDouble();
+        System.out.println("Enter income");
+        float income = inp.nextFloat();
+        System.out.println("The details entered are : \n||\n||");
+        System.out.println("Name:" + name);
+        System.out.printf("Age: %d%n", age);
+        System.out.printf("CGPA: %.3f%n", cgpa);
+        System.out.printf("Income: %.2f%n", income);
+        System.out.println("Thank You!");
+
+    }
+}
--- main.java
+++ main.java
@@ -1,6 +1,5 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
-        // This is a simple Java program that prints "Hello, World!" to the console.
         System.out.println("Hello, World!");
     }
 }
