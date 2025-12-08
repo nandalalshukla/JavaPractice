c3c58ffad7ded38d173c89026bf2d98e1dbe9477
Nandalal Shukla <shuklanandalal@gmail.com>
2025-12-09 03:39:23 +0530

    learnt 10 string methods and swithc case

---
+++ Four.java
@@ -0,0 +1,46 @@
+//goal -- learn various built-in string methods
+import java.util.Scanner;
+public class Four {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter any text and lets apply various string methods on it.");
+        System.out.println("Enter text1.");
+        String text1 = inp.nextLine();
+                System.out.println("Enter text2.");
+        String text2 = inp.nextLine();
+        System.out.println(
+                "Choose the method you want to use on the text.%n1: UPC%2: LWC %n3: equals%n 4: trim%n 5: split%n 6: replace %n 7: charat%n8: Length%n 9: concat %n______________________");
+        int choice = inp.nextInt();
+        switch (choice) {
+            case 1:
+        System.out.println(text1.toUpperCase());
+                break;
+                case 2:
+               System.out.println(text1.toLowerCase());
+                break;
+            case 3:
+                System.out.println("text1 and text2 are equal"+text1.equals(text2));
+                break;
+            case 4:
+                System.out.println("Trimmed text 1 is "+text1.trim());
+                break;
+            case 5:
+                System.out.println("The splitted text is :"+text1.split(" "));
+                break;
+            case 6:
+                System.out.println("text1 after replacing n with k is  "+ text1.replace('n','k'));
+                break;
+            case 7:
+                System.out.println("The character at index 5 is : "+text1.charAt(5));
+                break;
+            case 8:
+                System.out.println("The length of text 1 is : "+ text1.length());
+                break;
+            case 9:
+                System.out.println("The strings after concatenation is : "+ text1.concat(text2));
+                break;
+            default:
+                break;
+        }
+    }
+}
